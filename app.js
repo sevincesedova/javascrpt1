740df7d43f341aa42250090cb5632a179e6bc7d1
sevincesedova <161349938+sevincesedova@users.noreply.github.com>
2024-03-26 13:43:54 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// task1
+// let name = prompt("Adınızı qeyd edin:");
+// console.log(name.length);
+
+// task2
+// let word = prompt("sozu daxil edin:");
+// let letter = prompt("herfi daxil edin:");
+// console.log(word.includes(letter));
+
+// task3
+// let word = prompt("sozu daxil edin:");
+// let letter = prompt("herfi daxil edin:");
+// console.log(word1.startsWith(letter1));
+
+// task4
+// let word = prompt("sozu daxil edin:");
+// let letter = prompt("herfi daxil edin:");
+// console.log(word.endsWith(letter));
+
+// task5
+// let word = prompt("sozu daxil edin:");
+// let letter = prompt("herfi daxil edin:");
+// console.log(word.indexOf(letter))
+
+// task6
+let data = prompt("sozu daxil edin");
+let result= data.toUpperCase()
+console.log(data.includes(result));
